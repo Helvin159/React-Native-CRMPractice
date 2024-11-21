d3e692ca20d0a86f9ef44f3b84f83f2ea1984846
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2024-11-20 19:19:48 -0500

    update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [inputVal, setInputVal] = useState<string>('');
+
+  const handleChange = (value: string) => {
+    alert(value);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.heading}>CRM Dashboard</Text>
+      </View>
+      <View>
+        <TextInput
+          placeholder='Search'
+          style={styles.input}
+          value={inputVal}
+          onChangeText={handleChange}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button title='Click Here' color='#000' />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    padding: 16,
+    marginTop: 48,
+    alignItems: 'center'
   },
+  heading: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 600
+  },
+  button: {
+    backgroundColor: '#fff',
+    borderRadius: 10
+  },
+  input: {
+    padding: 16,
+    backgroundColor: '#fff',
+    marginBottom: 16,
+    borderRadius: 10
+  }
 });
